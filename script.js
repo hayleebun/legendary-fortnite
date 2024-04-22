26f9b2e24136d8aab635e02bfc14ed7d3f939bd9
crynchie <165821735+crynchie@users.noreply.github.com>
2024-04-21 20:24:01 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const video=document.getElementById('myVideo');
+
+function playVideo() {
+  video.play();
+}
+
+function pauseVideo() {
+  video.pause();
+}
+
+//We added video playback functions// 
