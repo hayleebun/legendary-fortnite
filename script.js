de2808ab601320e4312d8fe639f070abf39d68e6
crynchie <165821735+crynchie@users.noreply.github.com>
2024-05-08 14:48:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,29 +8,5 @@ function playVideo() {
 function pauseVideo() {
   video.pause();
 }
-//Email Form JS
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if(
-        !empty($_POST['name'])
-        && !empty($_POST['email'])
-        && !empty($_POST['message'])
-    ){
-        $name = $_POST["name"];
-        $email = $_POST["email"];
-        $message = $_POST["message"];
-
-
-        $to = "BLANK@YearUp.com";
-        $subject = "Contact Form Submission";
-        $body = "Name: {$name}\nEmail: {$email}\nPhone: {$phone}\nMessage: {$message}";
-        $headers = "From: {$email}";
-
-
-        if (mail($to, $subject, $body, $headers)) {
-            echo "Message sent successfully!";
-        } else {
-            echo "Failed to send message.";
-        }
-    }
 }
 //We added video playback functions// 
